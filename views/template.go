6d4c3700922b4f89d526b4afa5f387c9f78bd6a4
Rahul Choudhary <rahulrc7692@gmail.com>
2025-10-31 22:27:38 +0530

    Added Home views + static Handler

---
+++ views/template.go
@@ -0,0 +1,46 @@
+package views
+
+import (
+	"fmt"
+	"html/template"
+	"io/fs"
+	"log"
+	"net/http"
+	"path"
+)
+
+type Template struct {
+	htmlTpl *template.Template
+}
+
+func ParseFs(fs fs.FS, patterns ...string) (Template, error) {
+	tpl := template.New(path.Base(patterns[0]))
+	//Registers custom functions that can be called
+	//from within your HTML templates
+	//Funcs must be called before ParseFS/Parse
+	tpl.Funcs(
+		template.FuncMap{
+			"csrfField": func() template.HTML {
+				return `<input type="hidden">`
+			},
+		},
+	)
+
+	tpl, err := tpl.ParseFS(fs, patterns...)
+	if err != nil {
+		return Template{}, fmt.Errorf("parsing template: %w", err)
+	}
+	return Template{htmlTpl: tpl}, nil
+}
+
+// helper func to reuse for templates
+// Execute writes the tpl data as a response to the client
+func (t Template) Execute(w http.ResponseWriter, data interface{}) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	err := t.htmlTpl.Execute(w, data)
+	if err != nil {
+		log.Printf("parsing template: %v", err)
+		http.Error(w, "There was an error Executing the template", http.StatusInternalServerError)
+		return
+	}
+}
